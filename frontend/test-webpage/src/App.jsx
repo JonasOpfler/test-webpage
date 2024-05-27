7d2749397c5eab8b10c803dbe9dd7a60cf21785a
JonasOpfler <jonasopfler@gmail.com>
2024-05-27 15:24:19 +0200

    working-init

---
+++ frontend/test-webpage/src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import Analytics from './components/Analytics'
+import Newsletter from './components/Newsletter'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
+import BackToTopButton from './components/BackToTopButton'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
+      <BackToTopButton />
+
+
+    </div>
+  )
+}
+
+export default App
